68490c8da7ac0c88f95f6e423da4c6c1a0352997
JennAnd <89971271+JennAnd@users.noreply.github.com>
2021-12-27 18:33:42 +0100

    Duplicate user
    Solved the problem with the duplicate user everytime I create a new user.

--- app/users/register.php
+++ app/users/register.php
@@ -18,20 +18,11 @@ if (isset($_POST['name'], $_POST['email'], $_POST['password'], $_POST['confpassw
     }
 
 
-
-
-
     $statement = $database->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
     $statement->bindParam(':name', $name, PDO::PARAM_STR);
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->bindParam(':password', $password, PDO::PARAM_STR);
 
     $statement->execute();
-
-
-
-
-
-    $database->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
 }
-//redirect('/');//
+redirect('/');
