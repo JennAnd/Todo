6ac085fc23f1a9cb336eb876850c37e5d41c652a
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-01-12 21:24:02 +0100

    change index.php
    changed index.php home before log in.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
+
 <!--If user is logged in, the profile image displays.-->
 <?php if (isset($_SESSION['user']['profile_image'])) : ?>
     <img class="profile-image" src="/upload/<?php echo $_SESSION['user']['profile_image'] ?>" alt="users profile picture">
@@ -11,6 +12,10 @@
 <?php if (isset($_SESSION['user'])) : ?>
     <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!
     <p>It's time to organize your life! <br>Choose between the links in the navigation bar.</p>
+<?php else : ?>
+    <br>
+    <p>Welcome to To-Do!<br> Create an account or log in to start making your To-Do list.</p>
+    <img class="home-image" src="/assets/images/todo.jpg" alt="coffee and list on a table">
 <?php endif; ?>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
